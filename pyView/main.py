7b43f28ad7389954714c46d558dd9f06e1af6e57
Piotr Walas <walas.piotr.prywatny@gmail.com>
2021-11-12 23:50:35 +0000

    QT with embedded resizable videoFeed

--- pyView/main.py
+++ pyView/main.py
@@ -1,22 +1,53 @@
 import sys 
 
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QGridLayout
 from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtCore    import QRect
+from PyQt5.QtGui     import QPalette, QColor
 
-# Create an instance of QApplication
+from videoFeed import VidFeed
+
+class VideoDisplay(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.setGeometry(QRect(530, 20, 256, 192))
+        self.setObjectName("cameraWindow")
+        self.setAttribute(0, 1); # AA_ImmediateWidgetCreation == 0
+        self.setAttribute(3, 1); # AA_NativeWindow == 3
+        
+        self.setStyleSheet("background-color: black")
+
+    def get_id(self):
+        return self.winId()
+
+class MainWindow(QWidget):
+    
+    def __init__(self,display):
+        super().__init__()
+
+        self.setWindowTitle('PyView')
+        self.setGeometry(QRect(530, 20, 256, 192))
+
+        layout = QGridLayout()
+        layout.addWidget(display, 0, 1,3, 3)
+        layout.addWidget(QPushButton('Button0'), 0, 0)
+        layout.addWidget(QPushButton('Button0'), 1, 0)
+        
+        self.setLayout(layout)
+
 app = QApplication(sys.argv)
 
-# Create an instance of your application's GUI
-window = QWidget()
-window.setWindowTitle('PyQt5 App')
-window.setGeometry(100, 100, 280, 80)
-window.move(60, 15)
-helloMsg = QLabel('<h1>pyView</h1>', parent=window)
-helloMsg.move(60, 15)
 
-# Show your application's GUI
+display = VideoDisplay()
+window = MainWindow(display)
 window.show()
 
-# Run your application's event loop (or main loop)
+vid = VidFeed(display.get_id())
+vid.startPrev()
+
 sys.exit(app.exec_())
+
--- pyView/videoFeed.py
+++ pyView/videoFeed.py
@@ -7,37 +7,53 @@ from gi.repository import Gst, GObject, Gtk
 
 # Needed for window.get_xid(), xvimagesink.set_window_handle(), respectively:
 from gi.repository import GdkX11, GstVideo
+import sys 
+
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtCore    import QRect
 
 class VidFeed:
 
-    def __init__(self):
+    def __init__(self,windowId):
+        Gst.init(None)
         # gst-launch-1.0 v4l2src do-timestamp=TRUE device=/dev/video0 ! videoconvert ! xvimagesink
         self.player = Gst.Pipeline.new("player")
         self.source = Gst.ElementFactory.make("v4l2src", "vsource")
-        self.sink = Gst.ElementFactory.make("ximagesink","video-output")
+        self.conv   = Gst.ElementFactory.make("videoconvert", "colorspace")
+        self.scaler = Gst.ElementFactory.make("videoscale", "fvidscale")
+        self.sink   = Gst.ElementFactory.make("ximagesink","video-output")
+        
         self.source.set_property("device","/dev/video0")
-        self.colorspace = Gst.ElementFactory.make("videoconvert", "colorspace")
+        
+        self.__add_many([self.source,self.conv,self.scaler,self.sink])
+        self.__link_many([self.source,self.conv,self.scaler,self.sink])
 
-        self.player.add(self.source)
-        self.player.add(self.colorspace)
-        self.player.add(self.sink)
-
-        self.source.link(self.colorspace)
-        self.colorspace.link(self.sink)
+        self.__set_window_id(windowId)
 
         bus = self.player.get_bus()
         bus.add_signal_watch()
         bus.enable_sync_message_emission()
-        bus.connect("message", self.on_message)
-        bus.connect("sync-message::element", self.on_sync_message)
-        self.startPrev()
-    # def start_stop(self, w):
-    #     if self.button.get_label() == "Start":
-    #         
+        bus.connect("message", self.__on_message)
+        bus.connect("sync-message::element", self.__on_sync_message)
+        
 
-    def on_message(self, bus, message):
+        # self.startPrev()
+
+    def __set_window_id(self,windowId):
+        self.windowId = windowId
+
+    def __add_many(self,pipeline_list):
+        for node in pipeline_list:
+            self.player.add(node)
+
+    def __link_many(self,pipeline_list):
+        for n in range(len(pipeline_list)-1):
+            pipeline_list[n].link(pipeline_list[n+1])
+
+    def __on_message(self, bus, message):
         t = message.type
-        print(f"on_message: {message} type: {t}")
         if t == Gst.MessageType.EOS:
             self.player.set_state(Gst.State.NULL)
         elif t == Gst.MessageType.ERROR:
@@ -45,20 +61,32 @@ class VidFeed:
             print(f"Error: {err} ", debug)
             self.player.set_state(Gst.State.NULL)
 
-    def on_sync_message(self, bus, message):
-        print("on_sync_message")
+    def __on_sync_message(self, bus, message):
         if message.get_structure().get_name() == 'prepare-window-handle':
-            print("here i am")
+            win_id = self.windowId
             imagesink = message.src
             imagesink.set_property("force-aspect-ratio", True)
-            xid = self.movie_window.get_property('window').get_xid()
-            imagesink.set_window_handle(xid)
+            # if not window id then create new window
+            if win_id == None:
+                win_id = self.movie_window.get_property('window').get_xid()   
+            imagesink.set_window_handle(win_id)
 
     def startPrev(self):
         self.player.set_state(Gst.State.PLAYING)
-        print("should be playing")
+    
 
-GObject.threads_init()
-Gst.init(None)        
-VidFeed()
-Gtk.main()
+# main for internal tests
+if __name__=="__main__":
+
+    app = QApplication(sys.argv)
+    cameraWindow = QWidget()
+    cameraWindow.setGeometry(QRect(530, 20, 256, 192))
+    cameraWindow.setObjectName("cameraWindow")
+    cameraWindow.setAttribute(0, 1); # AA_ImmediateWidgetCreation == 0
+    cameraWindow.setAttribute(3, 1); # AA_NativeWindow == 3
+    cameraWindow.show()
+
+    wId = cameraWindow.winId()
+
+    VidFeed(wId)
+    sys.exit(app.exec_())
